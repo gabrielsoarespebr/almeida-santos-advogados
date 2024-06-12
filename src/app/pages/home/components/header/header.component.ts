ba084cdc286c21a26719b5a19087707496ce86bf
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-06-11 23:30:59 -0300

    refactor: Header background transition on scroll and nav text

--- src/app/pages/home/components/header/header.component.ts
+++ src/app/pages/home/components/header/header.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  backgroundColorOn: boolean = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollRef =
+      document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (scrollRef > 50) {
+      this.backgroundColorOn = true;
+    } else {
+      this.backgroundColorOn = false;
+    }
+  }
 }
